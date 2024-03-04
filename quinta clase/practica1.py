25ad1eb0d8ad5d1c1adfe93464c59ff51e1f648b
ShubaShuba <bgaitan@ucol.mx>
2024-03-04 12:48:00 -0600

    prueba de commit

---
+++ quinta clase/practica1.py
@@ -0,0 +1,13 @@
+import nltk
+
+
+carpeta = "C:\\Users\\pabli\\FIME\\"
+archivo_nombre = "archivo_texto.txt"
+
+with open(carpeta+archivo_nombre,"r") as archivo:
+    texto=archivo.read()
+
+tokens=nltk.word_tokenize(texto,"spanish")
+
+for token in tokens:
+    print(token)
