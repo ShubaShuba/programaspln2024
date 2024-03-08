529e16cf907a579948ac1a838fd1f05c7d602073
ShubaShuba <bgaitan@ucol.mx>
2024-03-08 11:43:28 -0600

    .

--- quinta clase/practica1.py
+++ quinta clase/practica1.py
@@ -9,8 +9,11 @@ with open(carpeta+archivo_nombre,"r") as archivo:
 
 tokens=nltk.word_tokenize(texto,"spanish")
 
-for token in tokens:
-    print(token)
+tokens_conjunto = set(tokens)
 
-palabras_Total=len(tokens)
-print("Total de palabras: ",palabras_Total)
+palabras_totales = len(tokens)
+palabras_unicas = len(tokens_conjunto)
+
+riqueza_lexica = palabras_unicas/palabras_totales
+
+print(riqueza_lexica)
