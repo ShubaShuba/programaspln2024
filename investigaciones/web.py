950c9aa8c29854f4789f574cc2013bf2025ee705
ShubaShuba <bgaitan@ucol.mx>
2024-04-15 15:22:25 -0600

    Web scraping con Python V1.2

--- investigaciones/web.py
+++ investigaciones/web.py
@@ -5,9 +5,9 @@ import nltk
 import urllib.request
 
 input("Quiere usar una Web Personalizada?")
-if input == "si":
+if input == "si" or input == "Si":
     url = input("Ingrese la url de la pagina web: ")
-else:
+else:      #la URL colocada puede que futuramente ya no exista por caducidad
     url = "https://pastebin.com/CJtDF6uQ"
 
 response = urllib.request.urlopen(url)
@@ -29,5 +29,16 @@ print(texto)
 tokens = nltk.word_tokenize(texto, "spanish")
 
 texto_nltk = nltk.Text(tokens)
-concordancia = input("Ingrese la palabra para buscar en el texto: ")
-print(texto_nltk.concordance(concordancia))
+
+buscar_palabra = input("Ingrese la palabra que desea buscar en la pagina web: ")
+num_palabras = texto_nltk.count(buscar_palabra)
+print("Numero de veces que aparece la palabra: ", num_palabras)
+
+num_palabras = len(texto_nltk)
+print("Numero de palabras: ", num_palabras)
+
+#esta funcion exacta no funciona correctamente debido a que la funcion que extrae los datos los obtiene de manera cruda sin seguir ningun formato
+num_lineas = texto_nltk.count("\n")
+print("Numero de lineas: ", num_lineas)
+
+
